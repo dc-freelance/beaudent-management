348a708a616a0c3e72e93edda252387fe1eaa71f
golz04 <muhammadhaidar862@gmail.com>
2024-02-13 18:17:59 +0700

    feat: setup laravel spatie

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        // Provider Spatie Permission for role and permission
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
