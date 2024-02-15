33a2b95fc0b840b7fc7fbb47c2c58cbb31b2f386
ardianhilmip35 <ardianhil86@gmail.com>
2024-02-15 14:07:29 +0700

    Registration API Done

---
+++ app/Models/Customers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Customers extends Model
+{
+    use HasFactory;
+
+    protected $table = 'customers';
+    use SoftDeletes;
+    protected $fillable = [
+        'name', 'date_of_birth',
+        'place_of_birth',
+        'identity_number',
+        'gender',
+        'occupation',
+        'phone_number',
+        'religion',
+        'email',
+        'marrital_status',
+        'oral_issues',
+        'note' ,
+        'instagram' ,
+        'youtube' ,
+        'facebook' ,
+        'source_of_information' 
+    ];
+    
+}
