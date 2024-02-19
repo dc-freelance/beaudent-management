7ffcd16b1a3d8146ca247a919e3f25a7b0e6679d
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-02-20 04:56:14 +0700

    feat: Add doctor category and doctor seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
             BranchSeeder::class,
             TreatmentSeeder::class,
             UserSeeder::class,
+            DoctorCategorySeeder::class,
+            DoctorSeeder::class,
             CustomerSeeder::class,
             PermissionSeeder::class,
             ItemCategorySeeder::class,
