e354c2ef84ff20967b15dc97054497b2f2cfcd00
chelotasnim <grumpfyxylozx@gmail.com>
2024-02-22 23:14:49 +0700

    fix: reservation number and reschedule button

--- resources/views/front-office/reservations/column/action.blade.php
+++ resources/views/front-office/reservations/column/action.blade.php
@@ -1,8 +1,8 @@
 <div class="lg:flex gap-x-2">
-    @if ($data->status === 'Done' )
+    @if ($data->status === 'Done')
         <a href="{{ route('front-office.reservations.confirm.reschedule', $data->id) }}"
             class="text-white bg-orange-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
-            Detail
+            Jadwal ulang
         </a>
         <a href="{{ route('front-office.reservations.confirm.detail', $data->id) }}"
             class="text-white bg-blue-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
@@ -20,7 +20,7 @@
         </a>
     @endif
 
-    
+
     <label onclick="btnDelete('{{ $data->id }}', '{{ $data->name }}')"
         class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
         Hapus
