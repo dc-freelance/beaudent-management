d5847847ef11d3937140565631786a1995ef8c31
alealien666 <andraale28@gamil.com>
2024-02-28 22:07:20 +0700

    form-components

--- resources/views/admin/customer/column/action.blade.php
+++ resources/views/admin/customer/column/action.blade.php
@@ -1,19 +1,19 @@
 <div class="lg:flex gap-x-2">
     @can('update_customer')
         <a href="{{ route('admin.customer.edit', $data->id) }}"
-            class="text-white bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
+            class="text-white bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center px-3 hover:bg-blue-600 transition duration-300 ease-in-out">
             Ubah
         </a>
     @endcan
     @can('detail_customer')
         <a href="{{ route('admin.customer.detail', $data->id) }}"
-            class="text-white bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
+            class="text-white bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center hover:bg-green-500 transition duration-300 ease-in-out">
             Detail
         </a>
     @endcan
     @can('delete_customer')
         <label onclick="btnDelete('{{ $data->id }}', '{{ $data->name }}')"
-            class="text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
+            class="text-white bg-red-500 hover:bg-red-600 transition duration-300 ease-in-out focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
             Hapus
         </label>
     @endcan
--- resources/views/admin/item-unit/column/action.blade.php
+++ resources/views/admin/item-unit/column/action.blade.php
@@ -1,13 +1,13 @@
 <div class="lg:flex gap-x-2">
     @can('update_item_unit')
         <a href="{{ route('admin.item-unit.edit', $data->id) }}"
-            class="text-white bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
+            class="text-white bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center px-3 hover:bg-blue-600 transition duration-300 ease-in-out">
             Ubah
         </a>
     @endcan
     @can('delete_item_unit')
         <label onclick="btnDelete('{{ $data->id }}', '{{ $data->name }}')"
-            class="text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
+            class="text-white bg-red-500 hover:bg-red-600 transition duration-300 ease-in-out focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
             Hapus
         </label>
     @endcan
