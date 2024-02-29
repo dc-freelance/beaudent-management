9ba53fd6a6485683c598d5c70c1606d9d0b5b1cd
29rifjanj <rjndla@gmail.com>
2024-02-29 17:14:51 +0700

    feat-laporan: fix report excel laporan

---
+++ app/Exports/TreatmentReportGeneralExport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exports;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+
+class TreatmentReportGeneralExport implements FromView
+{
+    private $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function view(): View
+    {
+        return view('admin.treatment_report.excel', ['data' => $this->data]);
+    }
+}
+?>
