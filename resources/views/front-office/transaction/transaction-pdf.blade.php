b7eb97f5453627c338698ea9e53c1d9c019b29fd
golz04 <muhammadhaidar862@gmail.com>
2024-03-01 01:16:57 +0700

    fix: print

--- resources/views/front-office/transaction/transaction-pdf.blade.php
+++ resources/views/front-office/transaction/transaction-pdf.blade.php
@@ -17,7 +17,7 @@
         <p>Ringkasan Transaksi</p>
         Total Biaya Pelayanan : <span style="float: right; clear: both;">{{ "Rp. ".number_format($detailExaminationTreatment->sum('sub_total'), 0, ',', '.') }} </span>              <br>
         Total Biaya Obat dan BMHP : <span style="float: right; clear: both;"> {{ "Rp. ".number_format($detailExaminationItem->sum('sub_total'), 0, ',', '.') }} </span>              <br>
-        Total Biaya Layanan Tambahan : <span style="float: right; clear: both;"> {{ "Rp. ".number_format($detailAddonTransaction->sum('addon.price'), 0, ',', '.') }} </span>        <br>
+        Total Biaya Layanan Tambahan : <span style="float: right; clear: both;"> {{ "Rp. ".number_format($detailAddonExamination->sum('sub_total'), 0, ',', '.') }} </span>        <br>
         <p></p>
         Total : <span style="float: right; clear: both;"> {{ "Rp. ".number_format($detailTransaction->total, 0, ',', '.') }} </span>                                                 <br>
         Diskon : <span style="float: right; clear: both;"> {{ "Rp. ".number_format($detailTransaction->discount, 0, ',', '.') }} </span>                                             <br>
