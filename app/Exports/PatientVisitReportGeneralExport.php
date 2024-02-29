d6f3292cd67a2f6d8919d1483827135b5b3d8d67
Okan Sajaaa <okannolgenol@gmail.com>
2024-02-29 17:10:22 +0700

    feat-laporan:add laporan kunjungan pasien

---
+++ app/Exports/PatientVisitReportGeneralExport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exports;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+
+class PatientVisitReportGeneralExport implements FromView
+{
+    private $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function view(): View
+    {
+        return view('admin.patient_visit_report.excel', ['data' => $this->data]);
+    }
+}
+?>
