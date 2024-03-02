aff7b5258bc2f4883a2027188cba6d8ff2d81195
golz04 <muhammadhaidar862@gmail.com>
2024-03-03 02:07:13 +0700

    fix: issue

--- resources/views/front-office/transaction/column/action.blade.php
+++ resources/views/front-office/transaction/column/action.blade.php
@@ -1,8 +1,8 @@
-@can('pay_antiran_pembayaran')
+{{-- @can('pay_antiran_pembayaran') --}}
     <div class="lg:flex gap-x-2">
         <a href="{{ route('front-office.transaction.payment', $data->id) }}"
             class="text-white bg-orange-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
             Bayar
         </a>
     </div>
-@endcan
+{{-- @endcan --}}
