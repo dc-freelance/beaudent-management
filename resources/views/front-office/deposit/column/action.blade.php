feb684294b008e486edd540c649cc05939f1a83d
chelotasnim <grumpfyxylozx@gmail.com>
2024-03-04 13:34:17 +0700

    fix: reservation action button wording

--- resources/views/front-office/deposit/column/action.blade.php
+++ resources/views/front-office/deposit/column/action.blade.php
@@ -1,24 +1,24 @@
 <div class="lg:flex gap-x-2">
-    @if ($data->status === 'Waiting Deposit')
+    {{-- @if ($data->status === 'Waiting Deposit')
         @can('reschedule_deposit')
             <a href="{{ route('front-office.reservations.confirm.reschedule', $data->id) }}"
                 class="text-white bg-blue-500 focus:ring-4 hover:bg-blue-600 transition duration-300 ease-in-out focus:outline-none focus:ring-blue-600 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
                 Jadwal ulang
             </a>
         @endcan
-    @endif
-    @if ($data->status === 'Pending Deposit')
-        @can('detail_deposit')
-            <a href="{{ route('front-office.deposit.wait.detail', $data->id) }}"
-                class="text-white bg-green-400 hover:bg-green-500 transition duration-300 ease-in-out focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
-                Detail
-            </a>
+    @endif --}}
+    @can('detail_deposit')
+        <a href="{{ route('front-office.deposit.wait.detail', $data->id) }}"
+            class="text-white bg-green-400 hover:bg-green-500 transition duration-300 ease-in-out focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center">
+            Detail
+        </a>
+    @endcan
+    @if ($data->status !== 'Waiting Deposit')
+        @can('delete_deposit')
+            <label onclick="btnDelete('{{ $data->id }}', '{{ $data->name }}')"
+                class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-600 transition duration-300 ease-in-out font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
+                Batal
+            </label>
         @endcan
     @endif
-    @can('delete_deposit')
-        <label onclick="btnDelete('{{ $data->id }}', '{{ $data->name }}')"
-            class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-600 transition duration-300 ease-in-out font-medium rounded-md text-sm p-2 text-center inline-flex items-center cursor-pointer">
-            Hapus
-        </label>
-    @endcan
 </div>
